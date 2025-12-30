5496560b97edc4bca6b8c20549733e10e272bf9e
Piyush Gaur <piyushgaur934@gmail.com>
2025-12-31 00:14:59 +0530

    Prepare Base Model Added

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
-from cnnClassifier import logger
-from cnnClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
-from cnnClassifier.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
-from cnnClassifier.pipeline.stage_03_model_training import ModelTrainingPipeline
-from cnnClassifier.pipeline.stage_04_model_evaluation import EvaluationPipeline
+from NephroVision import logger
+from NephroVision.pipeline.stage01_data_ingestion import DataIngestionTrainingPipeline
+from NephroVision.pipeline.stage02_prepare_base_model import (
+    PrepareBaseModelTrainingPipeline,
+)
+
+# from cnnClassifier.pipeline.stage_03_model_training import ModelTrainingPipeline
+# from cnnClassifier.pipeline.stage_04_model_evaluation import EvaluationPipeline
 
 
 def run_stage(stage_name: str, pipeline_class):
@@ -20,5 +23,5 @@ def run_stage(stage_name: str, pipeline_class):
 if __name__ == "__main__":
     run_stage("Data Ingestion stage", DataIngestionTrainingPipeline)
     run_stage("Prepare Base Model", PrepareBaseModelTrainingPipeline)
-    run_stage("Training", ModelTrainingPipeline)
-    run_stage("Evaluation stage", EvaluationPipeline)
+    # run_stage("Training", ModelTrainingPipeline)
+    # run_stage("Evaluation stage", EvaluationPipeline)
